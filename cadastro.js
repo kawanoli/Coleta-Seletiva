b75a8b5f622f1d46b779af5a2db4554259e60112
Kawan Oliveira <kawanvinicius73@gmail.com>
2023-11-13 22:14:16 -0300

    lop

--- cadastro.js
+++ cadastro.js
@@ -1,6 +1,8 @@
+//adiciona um evento no botão de cadastro para o clique:
 document.getElementById('btn-submit').addEventListener('click', function(event){
     event.preventDefault(); 
-// Pega os elementos de entrada
+
+// Pega os elementos de entrada e armazena em variáveis:
 var password = document.getElementById('password').value;
 var confirmasenha = document.getElementById('confirmasenha').value;
 var name = document.getElementById('name').value;
@@ -8,13 +10,15 @@ var lastname = document.getElementById('lastname').value;
 var email = document.getElementById('email').value;
 
 
-console.log(password, confirmasenha)
+console.log(password, confirmasenha) //printando os valores digitados pra verificar se o que digitou foi igual
 
+//verifica se os campos estão vazios:
 if (name === '' || lastname === '' || email === '' || password === '' || confirmasenha === '') {
     alert('Preencha todos os campos obrigatorios para cadastrar');
     return;
 }
 
+//verificação das senhas (verifica se a senha do campo principal é a mesma do campo de confirmação):
 if (password === confirmasenha) {
     console.log("funciona - cadastro realizado")
 }else {
--- login.js
+++ login.js
@@ -1,10 +1,12 @@
-//Verificação para validação
+//adiciona um evento no botão de login para o clique:
         document.getElementById('btnsublogin').addEventListener('click', function(event){
             event.preventDefault(); 
-    
+            
+            //guarda os valores digitados nos campos de usuário e senha em váriaveis
             var username = document.getElementById('username').value;
             var password = document.getElementById('password').value;
     
+            //pega as variáveis de user e senha e verifica se uma delas está vazia (verificação de campo vazio):
             if (username === '' || password === '') {
                 alert('Preencha todos os campos para realizar login');
                 return;
@@ -12,17 +14,12 @@
 
             console.log(username, password); //só pra mostrar que tá passando o login e senha
 
+            //verifica se o username e a senha são válidos:
             if (username === 'teste' && password === 'teste') {
                 alert('Login realizado!');
-                window.location.href = 'solicitacao.html';
+                window.location.href = 'solicitacao.html'; //caso user e senha válidos, envia para a página de solicitação
             }else {
                 alert('Usuário ou senha inválido. Tente novamente.');
             }
 
-            form.addEventListener("submit", (event) => {
-                // Se todos os campos forem válidos, redirecionar para a página inicial
-                if (isValid()) {
-                  window.location.href = "homepage.html";}
-                
-            });
         });
